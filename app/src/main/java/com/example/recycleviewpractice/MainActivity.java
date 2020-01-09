af9d1174bbe75073c03cf160818687e09b809e54
saeedmoin3 <firebase.project.saeed@gmail.com>
2020-01-09 11:15:17 +0600

    Catagory list added

--- app/src/main/java/com/example/recycleviewpractice/MainActivity.java
+++ app/src/main/java/com/example/recycleviewpractice/MainActivity.java
@@ -2,17 +2,19 @@ package com.example.recycleviewpractice;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.ActionMenuView;
 
+import com.example.recycleviewpractice.adapter.CatagoryAdapder;
 import com.example.recycleviewpractice.adapter.MovieAdapter;
 import com.example.recycleviewpractice.utils.DataGenerator;
 
 public class MainActivity extends AppCompatActivity {
 
-    RecyclerView recyclerView;
+    RecyclerView recyclerView, recyclerView2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,13 +22,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         recyclerView = findViewById(R.id.recyclerviewId);
+        recyclerView2 = findViewById(R.id.recyclerviewId2);
 
         RecyclerView.LayoutManager layoutManager = new GridLayoutManager(this,3);
         recyclerView.setLayoutManager(layoutManager);
 
+        LinearLayoutManager layoutManager2
+                = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+
+        recyclerView2.setLayoutManager(layoutManager2);
+
+
 
         MovieAdapter adapter = new MovieAdapter(this, DataGenerator.getProducts());
         recyclerView.setAdapter(adapter);
+
+        CatagoryAdapder adapder2 = new CatagoryAdapder(this, DataGenerator.getCatagory());
+        recyclerView2.setAdapter(adapder2);
 
     }
 
--- app/src/main/java/com/example/recycleviewpractice/utils/DataGenerator.java
+++ app/src/main/java/com/example/recycleviewpractice/utils/DataGenerator.java
@@ -1,6 +1,7 @@
 package com.example.recycleviewpractice.utils;
 
 import com.example.recycleviewpractice.R;
+import com.example.recycleviewpractice.model.Catagory;
 import com.example.recycleviewpractice.model.Product;
 
 import java.util.ArrayList;
@@ -11,26 +12,44 @@ public class DataGenerator {
 
         List<Product> productList = new ArrayList<>();
 
-        productList.add(new Product(1, R.drawable.ic_avengers,"Catagory 1"));
-        productList.add(new Product(2, R.drawable.ic_avengers,"Catagory 2"));
-        productList.add(new Product(3, R.drawable.ic_avengers,"Catagory 3"));
-        productList.add(new Product(4, R.drawable.ic_avengers,"Catagory 4"));
-        productList.add(new Product(5, R.drawable.ic_avengers,"Catagory 5"));
-        productList.add(new Product(6, R.drawable.ic_avengers,"Catagory 6"));
-        productList.add(new Product(6, R.drawable.ic_avengers,"Catagory 7"));
-        productList.add(new Product(6, R.drawable.ic_avengers,"Catagory 8"));
-        productList.add(new Product(6, R.drawable.ic_avengers,"Catagory 9"));
-        productList.add(new Product(6, R.drawable.ic_avengers,"Catagory 10"));
-        productList.add(new Product(6, R.drawable.ic_avengers,"Catagory 11"));
-        productList.add(new Product(6, R.drawable.ic_avengers,"Catagory 12"));
-        productList.add(new Product(6, R.drawable.ic_avengers,"Catagory 13"));
-        productList.add(new Product(6, R.drawable.ic_avengers,"Catagory 14"));
-        productList.add(new Product(6, R.drawable.ic_avengers,"Catagory 15"));
-        productList.add(new Product(6, R.drawable.ic_avengers,"Catagory 16"));
-        productList.add(new Product(6, R.drawable.ic_avengers,"Catagory 17"));
-        productList.add(new Product(6, R.drawable.ic_avengers,"Catagory 18"));
-        productList.add(new Product(6, R.drawable.ic_avengers,"Catagory 19"));
+        productList.add(new Product(1, R.drawable.ic_avengers,"Product 1"));
+        productList.add(new Product(2, R.drawable.ic_avengers,"Product 2"));
+        productList.add(new Product(3, R.drawable.ic_avengers,"Product 3"));
+        productList.add(new Product(4, R.drawable.ic_avengers,"Product 4"));
+        productList.add(new Product(5, R.drawable.ic_avengers,"Product 5"));
+        productList.add(new Product(6, R.drawable.ic_avengers,"Product 6"));
+        productList.add(new Product(6, R.drawable.ic_avengers,"Product 7"));
+        productList.add(new Product(6, R.drawable.ic_avengers,"Product 8"));
+        productList.add(new Product(6, R.drawable.ic_avengers,"Product 9"));
+        productList.add(new Product(6, R.drawable.ic_avengers,"Product 10"));
+        productList.add(new Product(6, R.drawable.ic_avengers,"Product 11"));
+        productList.add(new Product(6, R.drawable.ic_avengers,"Product 12"));
+        productList.add(new Product(6, R.drawable.ic_avengers,"Product 13"));
+        productList.add(new Product(6, R.drawable.ic_avengers,"Product 14"));
+        productList.add(new Product(6, R.drawable.ic_avengers,"Product 15"));
+        productList.add(new Product(6, R.drawable.ic_avengers,"Product 16"));
+        productList.add(new Product(6, R.drawable.ic_avengers,"Product 17"));
+        productList.add(new Product(6, R.drawable.ic_avengers,"Product 18"));
+        productList.add(new Product(6, R.drawable.ic_avengers,"Product 19"));
 
         return productList;
+    }
+
+    public static List<Catagory> getCatagory(){
+
+        List<Catagory> catagoryList = new ArrayList<>();
+        catagoryList.add(new Catagory(1,"Catagory 1"));
+        catagoryList.add(new Catagory(2,"Catagory 2"));
+        catagoryList.add(new Catagory(3,"Catagory 3"));
+        catagoryList.add(new Catagory(4,"Catagory 4"));
+        catagoryList.add(new Catagory(5,"Catagory 5"));
+        catagoryList.add(new Catagory(6,"Catagory 6"));
+        catagoryList.add(new Catagory(7,"Catagory 7"));
+        catagoryList.add(new Catagory(8,"Catagory 8"));
+        catagoryList.add(new Catagory(9,"Catagory 9"));
+        catagoryList.add(new Catagory(10,"Catagory 10"));
+
+
+    return catagoryList;
     }
 }
